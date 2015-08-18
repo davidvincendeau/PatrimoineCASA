94e764411976db18c3776a9bbd2316af51f1f43a
Bruce LANE <bruce@batchass.fr>
2015-08-18 15:38:05 +0200

    chemins vers images ng-src

--- www/js/services/histoires.js
+++ www/js/services/histoires.js
@@ -4,74 +4,74 @@ angular.module('casa').factory('Histoires', [ function() {
         id: 0,
         name: 'Decoupage chronologique',
         sousTitre: '',
-        url: '../www/pages/histoire/decoupage-chronologique.html',
-        vignette: '../www/img/historique.png'
+        url: 'pages/histoire/decoupage-chronologique.html',
+        vignette: 'img/historique.png'
     }, {
         id: 1,
         name: 'Traces pre/protohistoriques',
         sousTitre: 'Les traces pre et protohistoriques',
         url: '../www/pages/histoire/traces-pre-protohistoriques.html',
-        vignette: '../www/img/menhir.jpg'
+        vignette: 'img/menhir.jpg'
     }, {
         id: 2,
         name: 'Traces protohistoriques',
         sousTitre: 'Les traces protohistoriques',
-        url: '../www/pages/histoire/traces-protohistoriques.html',
-        vignette: '../www/img/enceinte_naouri.jpg'
+        url: 'pages/histoire/traces-protohistoriques.html',
+        vignette: 'img/enceinte_naouri.jpg'
     }, {
         id: 3,
         name: 'ANTIQUITE',
         sousTitre: 'Partie 1',
         url: '../www/pages/histoire/antiquite-1.html',
-        vignette: '../www/img/antiquite_opidium.jpg'
+        vignette: 'img/antiquite_opidium.jpg'
     }, {
         id: 4,
         name: 'ANTIQUITE',
         sousTitre: 'Partie 2',
         url: '../www/pages/histoire/antiquite-2.html',
-        vignette: '../www/img/saint_andrieu2.jpg'
+        vignette: 'img/saint_andrieu2.jpg'
     }, {
         id: 5,
         name: 'ANTIQUITE',
         sousTitre: 'Partie 3',
         url: '../www/pages/histoire/antiquite-3.html',
-        vignette: '../www/img/aqueduc.png'
+        vignette: 'img/aqueduc.png'
     }, {
         id: 6,
         name: 'ANTIQUITE',
         sousTitre: 'Partie 4',
         url: '../www/pages/histoire/antiquite-4.html',
-        vignette: '../www/img/enceinte_tourraque.jpg'
+        vignette: 'img/enceinte_tourraque.jpg'
     }, {
         id: 7,
         name: 'FEODALISATION',
         sousTitre: 'Partie 1',
         url: '../www/pages/histoire/feodalisation-1.html',
-        vignette: '../www/img/musee_picasso.jpg'
+        vignette: 'img/musee_picasso.jpg'
     }, {
         id: 7,
         name: 'FEODALISATION',
         sousTitre: 'Partie 2',
         url: '../www/pages/histoire/feodalisation-2.html',
-        vignette: '../www/img/chateau_villeneuve_loubet.jpg'
+        vignette: 'img/chateau_villeneuve_loubet.jpg'
     }, {
         id: 7,
         name: 'FEODALISATION',
         sousTitre: 'Partie 3',
         url: '../www/pages/histoire/feodalisation-3.html',
-        vignette: '../www/img/chateau_tourrette.jpg'
+        vignette: 'img/chateau_tourrette.jpg'
     }, {
         id: 7,
         name: 'FEODALISATION',
         sousTitre: 'Partie 4',
         url: '../www/pages/histoire/feodalisation-4.html',
-        vignette: '../www/img/chateau_rouret.jpg'
+        vignette: 'img/chateau_rouret.jpg'
     }, {
         id: 8,
         name: 'Traces pre/protohistoriques',
         sousTitre: 'Les traces pre et protohistoriques',
         url: '../www/pages/histoire/traces-pre-protohistoriques.html',
-        vignette: '../www/img/menhir.jpg'
+        vignette: 'img/menhir.jpg'
     }];
     return {
         all: function () {
--- www/js/services/paysages.js
+++ www/js/services/paysages.js
@@ -5,67 +5,67 @@ angular.module('casa').factory('Paysages', [ function() {
         name: 'Paysages',
         sousTitre: 'Introduction',
         url: '../www/pages/paysages/paysages-intro.html',
-        vignette: '../www/img/plateau2.jpg'
+        vignette: 'img/plateau2.jpg'
     }, {
         id: 1,
         name: 'Paysages',
         sousTitre: 'Les plateaux',
         url: '../www/pages/paysages/paysages-plateaux.html',
-        vignette: '../www/img/plateau1.jpg'
+        vignette: 'img/plateau1.jpg'
     }, {
         id: 2,
         name: 'Paysages',
         sousTitre: 'Les collines',
         url: '../www/pages/paysages/paysages-collines.html',
-        vignette: '../www/img/colline3.jpg'
+        vignette: 'img/colline3.jpg'
     }, {
         id: 3,
         name: 'Paysages',
         sousTitre: 'Le littoral',
         url: '../www/pages/paysages/paysages-littoral.html',
-        vignette: '../www/img/litto3b.jpg'
+        vignette: 'img/litto3b.jpg'
     }, {
         id: 4,
         name: 'Les espaces naturels',
         sousTitre: 'Introduction',
         url: '../www/pages/paysages/espaces-naturels-intro.html',
-        vignette: '../www/img/natura9.jpg'
+        vignette: 'img/natura9.jpg'
     }, {
         id: 5,
         name: 'Les espaces naturels',
         sousTitre: 'Natura 2000 terrestre',
         url: '../www/pages/paysages/espaces-naturels-natura2000.html',
-        vignette: '../www/img/natura4.jpg'
+        vignette: 'img/natura4.jpg'
     }, {
         id: 6,
         name: 'Les espaces naturels',
         sousTitre: 'Les parcs naturels',
         url: '../www/pages/paysages/espaces-naturels-parcs.html',
-        vignette: '../www/img/parcs5.jpg'
+        vignette: 'img/parcs5.jpg'
     }, {
         id: 7,
         name: 'Les espaces naturels',
         sousTitre: 'ZNIEFF terrestre',
         url: '../www/pages/paysages/espaces-naturels-znieff.html',
-        vignette: '../www/img/znieff.jpg'
+        vignette: 'img/znieff.jpg'
     }, {
         id: 8,
         name: 'L\'eau',
         sousTitre: 'Les axes bleus',
         url: '../www/pages/paysages/eau-axes.html',
-        vignette: '../www/img/bateau.jpg'
+        vignette: 'img/bateau.jpg'
     }, {
         id: 9,
         name: 'L\'eau',
         sousTitre: 'Le milieu marin',
         url: '../www/pages/paysages/eau-milieu-marin.html',
-        vignette: '../www/img/marin2.jpg'
+        vignette: 'img/marin2.jpg'
     }, {
         id: 10,
         name: 'L\'eau',
         sousTitre: 'Les phares',
         url: '../www/pages/paysages/eau-phares.html',
-        vignette: '../www/img/phare3.jpg'
+        vignette: 'img/phare3.jpg'
     }];
  
     return {
