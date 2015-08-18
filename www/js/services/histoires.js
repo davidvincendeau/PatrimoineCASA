0bfe3a0c0cbaa8478bba3fab7dbe80bbdac897a4
Bruce LANE <bruce@batchass.fr>
2015-08-18 10:16:25 +0200

    chemins vers images à revoir

--- www/js/services/histoires.js
+++ www/js/services/histoires.js
@@ -4,74 +4,74 @@ angular.module('casa').factory('Histoires', [ function() {
         id: 0,
         name: 'Decoupage chronologique',
         sousTitre: '',
-        url: '../pages/histoire/decoupage-chronologique.html',
-        vignette: '../img/historique.png'
+        url: '../www/pages/histoire/decoupage-chronologique.html',
+        vignette: '../www/img/historique.png'
     }, {
         id: 1,
         name: 'Traces pre/protohistoriques',
         sousTitre: 'Les traces pre et protohistoriques',
-        url: '../pages/histoire/traces-pre-protohistoriques.html',
-        vignette: '../img/menhir.jpg'
+        url: '../www/pages/histoire/traces-pre-protohistoriques.html',
+        vignette: '../www/img/menhir.jpg'
     }, {
         id: 2,
         name: 'Traces protohistoriques',
         sousTitre: 'Les traces protohistoriques',
-        url: '../pages/histoire/traces-protohistoriques.html',
-        vignette: '../img/enceinte_naouri.jpg'
+        url: '../www/pages/histoire/traces-protohistoriques.html',
+        vignette: '../www/img/enceinte_naouri.jpg'
     }, {
         id: 3,
         name: 'ANTIQUITE',
         sousTitre: 'Partie 1',
-        url: '../pages/histoire/antiquite-1.html',
-        vignette: '../img/antiquite_opidium.jpg'
+        url: '../www/pages/histoire/antiquite-1.html',
+        vignette: '../www/img/antiquite_opidium.jpg'
     }, {
         id: 4,
         name: 'ANTIQUITE',
         sousTitre: 'Partie 2',
-        url: '../pages/histoire/antiquite-2.html',
-        vignette: '../img/saint_andrieu2.jpg'
+        url: '../www/pages/histoire/antiquite-2.html',
+        vignette: '../www/img/saint_andrieu2.jpg'
     }, {
         id: 5,
         name: 'ANTIQUITE',
         sousTitre: 'Partie 3',
-        url: '../pages/histoire/antiquite-3.html',
-        vignette: '../img/aqueduc.png'
+        url: '../www/pages/histoire/antiquite-3.html',
+        vignette: '../www/img/aqueduc.png'
     }, {
         id: 6,
         name: 'ANTIQUITE',
         sousTitre: 'Partie 4',
-        url: '../pages/histoire/antiquite-4.html',
-        vignette: '../img/enceinte_tourraque.jpg'
+        url: '../www/pages/histoire/antiquite-4.html',
+        vignette: '../www/img/enceinte_tourraque.jpg'
     }, {
         id: 7,
         name: 'FEODALISATION',
         sousTitre: 'Partie 1',
-        url: '../pages/histoire/feodalisation-1.html',
-        vignette: '../img/musee_picasso.jpg'
+        url: '../www/pages/histoire/feodalisation-1.html',
+        vignette: '../www/img/musee_picasso.jpg'
     }, {
         id: 7,
         name: 'FEODALISATION',
         sousTitre: 'Partie 2',
-        url: '../pages/histoire/feodalisation-2.html',
-        vignette: '../img/chateau_villeneuve_loubet.jpg'
+        url: '../www/pages/histoire/feodalisation-2.html',
+        vignette: '../www/img/chateau_villeneuve_loubet.jpg'
     }, {
         id: 7,
         name: 'FEODALISATION',
         sousTitre: 'Partie 3',
-        url: '../pages/histoire/feodalisation-3.html',
-        vignette: '../img/chateau_tourrette.jpg'
+        url: '../www/pages/histoire/feodalisation-3.html',
+        vignette: '../www/img/chateau_tourrette.jpg'
     }, {
         id: 7,
         name: 'FEODALISATION',
         sousTitre: 'Partie 4',
-        url: '../pages/histoire/feodalisation-4.html',
-        vignette: '../img/chateau_rouret.jpg'
+        url: '../www/pages/histoire/feodalisation-4.html',
+        vignette: '../www/img/chateau_rouret.jpg'
     }, {
         id: 8,
         name: 'Traces pre/protohistoriques',
         sousTitre: 'Les traces pre et protohistoriques',
-        url: '../pages/histoire/traces-pre-protohistoriques.html',
-        vignette: '../img/menhir.jpg'
+        url: '../www/pages/histoire/traces-pre-protohistoriques.html',
+        vignette: '../www/img/menhir.jpg'
     }];
     return {
         all: function () {
