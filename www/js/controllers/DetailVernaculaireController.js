a5f8fc6b11556ed1937fa743a9d3c69ef648e2fa
Kabia Edouard <dadi.kabia@gmail.com>
2015-09-02 16:51:48 +0200

    menu

---
+++ www/js/controllers/DetailVernaculaireController.js
@@ -0,0 +1,13 @@
+angular.module('casa').controller('DetailVernaculaireController',
+  [ '$scope',
+  	'$stateParams',
+    'Vernaculaires',
+    function(
+      $scope,
+      $stateParams,
+      Vernaculaires
+      ) {
+      
+     $scope.vernaculaire = Vernaculaires.get($stateParams.vernaculaireId);
+
+    }]);
