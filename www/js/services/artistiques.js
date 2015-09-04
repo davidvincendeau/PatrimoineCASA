c7560c5265a9f726976e5b23ca5c4a01fc15ebd8
Kabia Edouard <dadi.kabia@gmail.com>
2015-09-04 16:31:37 +0200

    artistique et contemporain

--- www/js/services/artistiques.js
+++ www/js/services/artistiques.js
@@ -2,33 +2,33 @@ angular.module('casa').factory('Artistiques', [ function() {
 
       var artistiques = [{
         id: 0,
-        name: 'Decoupage chronologique',
-        sousTitre: '',
-        url: 'pages/histoire/decoupage-chronologique.html',
+        name: 'La matière',
+        sousTitre: 'La poterie et la céramique',
+        url: 'pages/artistique/artistique-matiere-poterie.html',
         vignette: 'img/historique.png'
     }, {
         id: 1,
-        name: 'Traces pre/protohistoriques',
-        sousTitre: 'Les traces pre et protohistoriques',
-        url: 'pages/histoire/traces-pre-protohistoriques.html',
+        name: 'La matière',
+        sousTitre: 'la verrerie d\'art',
+        url: 'pages/artistique/artistique-matiere-verrerie.html',
         vignette: 'img/menhir.jpg'
     }, {
         id: 2,
-        name: 'Traces protohistoriques',
-        sousTitre: 'Les traces protohistoriques',
-        url: 'pages/histoire/traces-protohistoriques.html',
+        name: 'La matière',
+        sousTitre: 'La peinture',
+        url: 'pages/artistique/artistique-matiere-peinture.html',
         vignette: 'img/enceinte_naouri.jpg'
     }, {
         id: 3,
         name: 'Antiquité',
         sousTitre: 'Partie 1',
-        url: 'pages/histoire/antiquite-1.html',
+        url: 'pages/artistique/antiquite-1.html',
         vignette: 'img/antiquite_opidium.jpg'
     }, {
         id: 4,
         name: 'Antiquité',
         sousTitre: 'Partie 2',
-        url: 'pages/histoire/antiquite-2.html',
+        url: 'pages/artistique/antiquite-2.html',
         vignette: 'img/saint_andrieu2.jpg'
     }, {
         id: 5,
